2f832fb7c2df7507378501e162e22f5d7130d003
Afawuah-kofi <kafawuah@st.ug.edu.gh>
2024-07-05 20:01:33 +0000

    Interface Completed

---
+++ Interface/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// Define the IMovable interface
+interface IMovable
+{
+    void Move();
+}
+
+// Car class implementing IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Bicycle class implementing IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Interfaces Example");
+
+        // Create instances of Car and Bicycle
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        // Call Move() method on each instance
+        Console.WriteLine("Moving the car:");
+        car.Move();
+
+        Console.WriteLine("\nMoving the bicycle:");
+        bicycle.Move();
+    }
+}
